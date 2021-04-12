52d258b1bdcd9b767f5c2ccd0c401b2b702e381b
thirusparks <thirusparks@gmail.com>
2021-04-13 02:10:46 +0530

    minion final commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var input = document.querySelector("#textArea")
+var output = document.querySelector("#outputText");
+
+var serverURL= "https://api.funtranslations.com/translate/minion.json";
+//var serverURL= "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationURL (inputText){
+    return serverURL + "?" + "text="+ inputText; // creating API url
+}
+function errorHandler(error){
+    console.log("Error occured" + error);
+    alert("Please try again after sometime")
+}
+function clickHandler(){
+    var inputText = input.value; // getting input
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json =>{
+            var translatedText = json.contents.translated;  // fetching output
+            output.innerText = translatedText // sending output
+        })
+        .catch(errorHandler)
+}
+btnTranslate.addEventListener('click', clickHandler);
